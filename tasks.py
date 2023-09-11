e25aad100368703f2e5c1097c427863dabe225fa
Mostafa Ahmadi <mostafa@AP-W-DF7MRQ3.yorku.yorku.ca>
2023-09-11 18:37:21 -0400

    first commit

---
+++ tasks.py
@@ -0,0 +1,31 @@
+from RPA.Browser.Selenium import Selenium
+import time
+browser_lib = Selenium()
+
+
+def open_the_website(url):
+    browser_lib.open_available_browser(url)
+
+
+def search_for(term):
+    input_field = "css:input"
+    browser_lib.input_text(input_field, term)
+    browser_lib.press_keys(input_field, "ENTER")
+
+
+def store_screenshot(filename):
+    browser_lib.screenshot(filename=filename)
+
+
+# Define a main() function that calls the other functions in order:
+def main():
+    open_the_website("https://robocorp.com/docs/")
+    search_for("python")
+    time.sleep(4)
+    store_screenshot("output/screenshot.png")
+    browser_lib.close_all_browsers()
+
+
+# Call the main() function, checking that we are running as a stand-alone script:
+if __name__ == "__main__":
+    main()
